5485f82d7ac12ba36f2d30bfbcf8c0cf91600dc2
Daniel Quintero <danielquintero500@gmail.com>
2025-06-19 23:00:57 +0000

    Daniel_Branch

--- moduloValidacionModelo.py
+++ moduloValidacionModelo.py
@@ -1,7 +1,8 @@
+from sklearn.metrics import accuracy_score # Libreria requerida para su ejecucion
+
 def evaluar_modelo(modelo, X_test, y_test, nombre_modelo="Modelo"): 
 """Se realizan la entrada de los valores para ejecutar la funcion, en este caso son los datos de testeo de entrada y de salida asi como el nombre del modelo el cual se va a ejecutar
 """
-    from sklearn.metrics import accuracy_score # Libreria requerida para su ejecucion
     y_pred = modelo.predict(X_test) # Se realiza el ingreso de la entrada de los datos de testeo
     accuracy = accuracy_score(y_test, y_pred) # Se calcula la presicion del modelo con los datos ingresados
     print(f"Precisión del {nombre_modelo}: {accuracy:.4f}") # Se realiza la impresion de los resultados
