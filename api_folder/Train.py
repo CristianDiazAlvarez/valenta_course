e3ec48a3cccae6b9279cc0665711d6b18bca257c
Jan S <jankevsaa@hotmail.com>
2025-06-25 00:03:22 +0000

    Al que le toco papa

--- api_folder/Train.py
+++ api_folder/Train.py
@@ -3,6 +3,7 @@ from clean_function import clean_data
 from Preprocesamiento import split_data
 from df_train import train_models
 from moduloValidacionModelo import evaluar_modelo
+import joblib
 import sys
 
 
@@ -17,10 +18,18 @@ def train_models2(filename=""):
 
     accuracy_models=evaluar_modelo(models,X_test,y_test)
 
-    print(df.head())
-    print(len(X_train),len(X_test),len(y_train),len(y_test))
-    print(models)
-    print(accuracy_models)
+    # ...existing code...
+
+    # Save the Random Forest model as a .pkl file
+    with open("model.pkl", "wb") as f:
+        joblib.dump(models["random_forest"], f)
+    
+    # ...existing code...
+
+    #print(df.head())
+    #print(len(X_train),len(X_test),len(y_train),len(y_test))
+    #print(models)
+    #print(accuracy_models)
 
 if __name__== "__main__":
     if len(sys.argv) > 1:
