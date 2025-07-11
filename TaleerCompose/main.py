8d5ded1cee555f94077da0241b9b41f63cc57a20
cs <kmilodelosantos@gmail.com>
2025-07-11 01:07:07 +0000

    update api

--- TaleerCompose/main.py
+++ TaleerCompose/main.py
@@ -1,15 +1,56 @@
-from fastapi import FastAPI
-from sklearn.datasets import load_iris
-from sklearn.ensemble import RandomForestClassifier
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import joblib
+import numpy as np
+import pandas as pd
+import os
 
 app = FastAPI()
 
-# Modelo simple de ejemplo
-data = load_iris()
-model = RandomForestClassifier()
-model.fit(data.data, data.target)
+# Load the pre-trained model
+#model = joblib.load("model.pkl")
 
-@app.get("/predict")
-def predict(sepal_length: float, sepal_width: float, petal_length: float, petal_width: float):
-    prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
-    return {"prediction": int(prediction[0])}
+# Definiendo un modelo de datos
+class Item(BaseModel):
+    Age: int = 40
+    Gender: int = 1
+    Polyur1a: bool = 0
+    Polydipsia: bool = 1
+    suddn_weight_loss: bool = 0
+    wea_kness: bool = 1
+    Polyphagia: bool = 0
+    Genital_thrush: bool = 0
+    visual_blurring: bool = 0
+    Itching: bool = 1
+    Irritability: bool = 0
+    delayed_healing: bool = 1
+    partial_paresis: bool = 0
+    muscle_stiffness: bool = 1
+    Alopecia: bool = 1
+    Obesity: bool = 1
+
+
+@app.get("/checkmodels")
+def check_models():
+
+    # Ruta del directorio que quieres explorar
+    directorio = '/app/notebooks'
+
+    # Listar y filtrar archivos que terminan en .pkl
+    archivos_pkl = [f for f in os.listdir(directorio) if f.endswith('.pkl')]
+
+    return archivos_pkl
+ 
+
+@app.post("/predict")
+def predict_item(item: Item):
+    try:
+        features = item.dict()
+        # Convertir a DataFrame de una sola fila
+        input_df = pd.DataFrame([features])
+        prediction = model.predict(input_df)
+        category = int(prediction[0])
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    return {"predicted_category": category}
