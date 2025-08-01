555392dd24518ec50918fbebd26d1ce691a06991
Cristian Diaz <cristiandiaz1802@gmail.com>
2025-07-31 19:09:20 -0500

    add pycaret

--- Niveles/2/Airflow/dags/4_dependencies.py
+++ Niveles/2/Airflow/dags/4_dependencies.py
@@ -14,7 +14,7 @@ with DAG (dag_id="4-dependencies",
     t1 = PythonOperator (task_id="tarea1",
                          python_callable=print_hello)
     t2 = BashOperator (task_id="tarea2",
-                       bash_command="echo 'tarea2")
+                       bash_command="echo 'tarea2'")
     t3 = BashOperator (task_id="tarea3",
                        bash_command="echo 'tarea3'")
     t4 = BashOperator (task_id="tarea4",
